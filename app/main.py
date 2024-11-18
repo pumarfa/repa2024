6116ab846049a151b50131d03e7af8a937844832
pumarfa <pumarfa@gmail.com>
2024-11-18 10:52:15 -0300

    Clase numero 5 primeros pasos de FastAPI. Response y manejo de datos.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,30 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 app.title = "REPA IAViM - 2024"
 app.version = "0.0.1"
 
+#
+# Listado de películas de ejemplo
+#
+
+dbmovies = [
+        {
+        "id":1,
+        "title":"Avatar",
+        "overview":"En un exuberante planeta llamado pandora",
+        "year":"2009",
+        "rating":7.8,
+        "category":"Accion"
+        }
+        ]
+
 @app.get('/', tags=['Home'])
 def message():
     return "Hello World!"
+
+@app.get('/movies', tags=['Movies'])
+def movies():
+    return dbmovies 
+
