91b3eface9da831aa02a6930bb2e14637e9c816a
pumarfa <pumarfa@gmail.com>
2024-11-19 17:15:15 -0300

    Clase 10. Modelos de datos para las entradas y salidas. La definifción de modelos de datos permite definir en un unico lugar la estructura de datos de un objeto, para ser utilizado en las tistintas funciones de las rutas, en especial en los Request body

--- app/main.py
+++ app/main.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from typing import Optional, List
 
 app = FastAPI()
 app.title = "REPA IAViM - 2024"
 app.version = "0.0.1"
 
+class Movie (BaseModel):
+    id: int
+    title: str
+    overview: str
+    year: int
+    rating: float
+    category: str
+
+class MovieUpdate (BaseModel):
+    title: str
+    overview: str
+    year: int
+    rating: float
+    category: str
 #
 # Listado de películas de ejemplo
 #
@@ -33,18 +49,18 @@ def message():
     return "Hello World!"
 
 @app.get('/movies', tags=['Movies'])
-def get_movies():
+def get_movies()->List[Movie]:
     return dbmovies 
 
 @app.get('/movies/{id}', tags=['Movies'])
-def get_movie(id: int):
+def get_movie(id: int)-> Movie:
     for movie in dbmovies:
         if movie['id'] == id:
             return movie
     return []
 
 @app.get('/movies/', tags=['Movies'])
-def get_movie_by_category(category: str, year: int):
+def get_movie_by_category(category: str, year: int)->Movie:
     for movie in dbmovies:
         if movie['category'] == category:
             return movie
@@ -52,33 +68,25 @@ def get_movie_by_category(category: str, year: int):
 
 # Clase 8
 @app.post('/movies', tags=['Movies'])
-def create_movie(id: int = Body(), title: str = Body(), overview: str = Body(), year: int = Body(), rating: float = Body(), category: str = Body()):
-    dbmovies.append({
-        "id": id,
-        "title": title,
-        "overview": overview,
-        "year": year,
-        "rating":rating,
-        "category":category 
-        })
-    
+def create_movie(movie:Movie)->List[Movie]:
+    dbmovies.append(movie.model_dump())
     return dbmovies 
 
 # clase 9 - Método PUT y DELETE
 @app.put('/movies/{id}', tags=['Movies'])
-def update_movie(id: int, title: str = Body(), overview: str = Body(), year: int = Body(), rating: float = Body(), category: str = Body()):
-    for movie in dbmovies:
-        if movie['id'] == id:
-            movie['title'] = title
-            movie['overview'] = overview 
-            movie['year'] = year 
-            movie['rating'] = rating 
-            movie['category'] = category 
+def update_movie(id: int, movie:MovieUpdate)->List[Movie]:
+    for item in dbmovies:
+        if item['id'] == id:
+            item['title'] = movie.title
+            item['overview'] = movie.overview 
+            item['year'] = movie.year 
+            item['rating'] = movie.rating 
+            item['category'] = movie.category 
 
     return dbmovies        
     
 @app.delete('/movies/{id}', tags=['Movies'])
-def delete_movie(id: int ):
+def delete_movie(id: int )->Movie:
     for movie in dbmovies:
         if movie['id'] == id:
             dbmovies.remove(movie)
