fdfca539f0e2359c11245a6cad51f6f5f0fb7bf3
pumarfa <pumarfa@gmail.com>
2024-11-20 18:52:21 -0300

    Clase 12 - Validaciones de parametros PATH y refactorización para usar los datos de películas como objetos

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Path, Query
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel, Field
 from typing import Optional, List
@@ -39,11 +39,11 @@ class MovieCreate (BaseModel):
         }
 
 class MovieUpdate (BaseModel):
-    title: str =Field(min_length=5, max_length=15)
-    overview: str = Field(min_length=0, max_length=150)
-    year: int = Field(ge=1900, le=datetime.date.today().year)
-    rating: float = Field(ge=0, le=10)
-    category: str = Field(min_length=0, max_length=30)
+    title: str
+    overview: str
+    year: int
+    rating: float
+    category: str
 
 #
 # Listado de películas de ejemplo
@@ -66,21 +66,21 @@ def get_movies()->List[Movie]:
 # Obtener un objeto con una pelicula por su ID
 #
 @app.get('/movies/{id}', tags=['Movies'])
-def get_movie(id: int)-> Movie:
+def get_movie(id: int= Path(ge=0))-> Movie | dict:
     for movie in dbmovies:
-        if movie['id'] == id:
-            return movie.model_dump
-    return []
+        if movie.id == id:
+            return movie.model_dump()
+    return {}
 
 #
 # Obtener una objeto con una película por su categoria
 #
 @app.get('/movies/', tags=['Movies'])
-def get_movie_by_category(category: str, year: int)->Movie:
+def get_movie_by_category(category: str= Query(min_length=0, max_length=30))-> Movie | dict:
     for movie in dbmovies:
-        if movie['category'] == category:
-            return movie.model_dump
-    return []
+        if movie.category == category:
+            return movie.model_dump()
+    return {}
 
 # Clase 8
 # Crear un objeto pelicula e insertarlo en la base de datos
@@ -108,7 +108,7 @@ def update_movie(id: int, movie:MovieUpdate)->List[Movie]:
 # Buscar un objeto película en la base de datos y borrrlo
 #
 @app.delete('/movies/{id}', tags=['Movies'])
-def delete_movie(id: int )->Movie:
+def delete_movie(id: int=Path(ge=0) )->Movie:
     for movie in dbmovies:
         if movie['id'] == id:
             dbmovies.remove(movie)
